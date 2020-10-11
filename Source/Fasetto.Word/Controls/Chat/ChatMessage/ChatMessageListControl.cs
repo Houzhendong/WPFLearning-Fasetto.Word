0c2610080e92c8cf4d6cd706a643b262f99345fa
WengWeng Hou <mr.houzhendong@gmail.com>
2020-10-11 10:56:09 +0800

    Finished P14:Create Chat Message Bubbles

---
+++ Source/Fasetto.Word/Controls/Chat/ChatMessage/ChatMessageListControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// ChatListControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class ChatMessageListControl : UserControl
+    {
+        public ChatMessageListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
