2a086acbccd97c54890e1266c30e72690da9387d
WengWeng Hou <mr.houzhendong@gmail.com>
2020-11-07 14:44:57 +0800

    Finished P23:Setting Page UI

---
+++ Source/Fasetto.Word/AttachedProperties/PanelChildMargin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// the isbusy attached property for anything that want to flag if the control is busy
+    /// </summary>
+    public class PanelChildMargin : BaseAttachedProperties<PanelChildMargin, string> 
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var panel = (sender as Panel);
+
+            panel.Loaded += (s, ee) =>
+            {
+                foreach (FrameworkElement child in panel.Children)
+                {
+                    (child as FrameworkElement).Margin = (Thickness)(new ThicknessConverter().ConvertFromString(e.NewValue as string));
+                }
+            };
+        }
+    }
+}
